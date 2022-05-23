c033c017f5baae6b0907450e85f46792e2ffbe74
irinainina <irbris@mail.ru>
2022-05-23 10:43:56 +0300

    fex: fix static function

--- pages/scoreboard.js
+++ pages/scoreboard.js
@@ -2,7 +2,7 @@ import styles from '../styles/ScoreBoard.module.scss';
 import Head from 'next/head';
 import Link from 'next/link';
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const url =
     'https://drzbiexu.api.sanity.io/v2021-10-21/data/query/production?query=*[_type==%27scores%27]';
   const response = await fetch(url);
