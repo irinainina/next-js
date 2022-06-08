97364d1a173ce478e7911f25f2867c9ce02192cc
irinainina <irbris@mail.ru>
2022-06-08 16:42:04 +0300

    feat: add twitter auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import FacebookProvider from "next-auth/providers/facebook";
+import TwitterProvider from "next-auth/providers/twitter";
 
 export default NextAuth({
   providers: [
@@ -16,6 +17,10 @@ export default NextAuth({
     FacebookProvider({
       clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET
+    }),
+    TwitterProvider({
+      clientId: process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_TWITTER_CLIENT_SECRET
     })
   ],
   theme: {
